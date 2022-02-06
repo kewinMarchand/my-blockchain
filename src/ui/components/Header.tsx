69cf4b595f192b3362bfe6cf37eedea38d0697d4
kewinMarchand <k.marchand@purjus.fr>
2022-02-06 18:29:55 +0100

    favicon

--- src/ui/components/Header.tsx
+++ src/ui/components/Header.tsx
@@ -6,13 +6,21 @@ export const Header = (): ReactElement => {
     const {myCoin} = useContext(AppContext);
 
     return (
-        <Box display={'flex'} component={'header'} justifyContent={'center'} alignItems={'center'} bgcolor={'primary.main'} gap={2} sx={{height: 72}}>
-            <picture>
-                <img src={myCoin?.icon} alt={"logo"} height={48} width={48} className={'spin'}/>
-            </picture>
-            <Typography variant={'h1'} color={"white"} data-testid={'appTitle'}>
-                My coin
-            </Typography>
+        <Box display={'flex'} component={'header'} justifyContent={'space-between'} alignItems={'center'} bgcolor={'primary.main'} px={3} sx={{height: 72}}>
+            <Box display={'flex'} alignItems={'center'} gap={2}>
+                <picture>
+                    <img src={myCoin?.icon} alt={"logo"} height={48} width={48} className={'spin'}/>
+                </picture>
+                <Typography variant={'h1'} color={"white"} data-testid={'appTitle'}>
+                    My coin
+                </Typography>
+            </Box>
+            <Box display={'flex'} alignItems={'center'} gap={1}>
+                <Typography variant={'h6'} color={"white"} data-testid={'appTitle'}>
+                    Total chain value : {myCoin?.chainValue}
+                </Typography>
+                <img src={myCoin?.icon} alt={'token'} height={20} width={20}/>
+            </Box>
         </Box>
     );
 }
